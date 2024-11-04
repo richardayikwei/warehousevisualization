122c167915c12510ff6347326972194e2b09cc42
Richard Ayikwei <richardayikweibee@gmail.com>
2024-11-04 04:50:56 +0000

    adding fetch and graph

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,51 @@
 import Header from "./components/Header";
 import ChartCard from "./components/ChartCard";
 import { Bar } from "react-chartjs-2";
-import "chart.js/auto";
+import { useEffect, useState } from "react";
+import { apiKey } from "./apikey/apikey";
+import { Colors, Chart } from "chart.js/auto";
+
+Chart.register(Colors);
+
 
 function App() {
- 
-  
+  const [inventory, setInventory] = useState([]);
+
+  useEffect(() => {
+    async function fetchInventory() {
+      const spreadsheetId = "1_y_fzDlQmBg8bVK2WBE8hD09imlPPEusITYCSZBvGdc";
+      const range = "inventory!A:C";
+      const url = `https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}/values/${range}?key=${apiKey}`;
+      try {
+        const res = await fetch(url);
+        if (!res.ok) {
+          throw Error("Inventory not retrieved");
+        }
+        const data = await res.json();
+        setInventory(data.values);
+      } catch (e) {
+        console.error(e);
+      }
+    }
+    fetchInventory();
+  }, []);
+
+  console.log(inventory);
   return (
     <div className="font-serif text-lg">
       <Header />
       <ChartCard>
         <Bar
           data={{
-            labels: [
-              "January",
-              "February",
-              "March",
-              "April",
-              "May",
-              "June",
-              "July",
-            ],
+            labels: inventory
+              .map((item) => item[0])
+              .filter((entry) => !(entry === "Item")),
             datasets: [
               {
-                label: "My First Dataset",
-                data: [65, 59, 80, 81, 56, 55, 40],
-                backgroundColor: "rgba(75, 192, 192, 0.2)",
-                borderColor: "rgba(75, 192, 192, 1)",
-                borderWidth: 1,
+                label: "Quantity",
+                data: inventory
+                  .map((item) => item[1])
+                  .filter((entry) => !(entry === "Quantity")),
               },
             ],
           }}
